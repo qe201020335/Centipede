5920fcdcb8ea355d453ef4b8dbd67b66016d2a86
Sky Z <qe201020335@sina.com>
2021-03-28 01:25:52 -0400

    centipide preamble

---
+++ centipede.s
@@ -0,0 +1,28 @@
+#####################################################################
+#
+# CSC258H Winter 2021 Assembly Final Project # University of Toronto, St. George
+#
+# Student: Name, Student Number
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 8
+# - Unit height in pixels: 8
+# - Display width in pixels: 256
+# - Display height in pixels: 256
+# - Base Address for Display: 0x10008000 ($gp)
+#
+# Which milestone is reached in this submission?
+# (See the project handout for descriptions of the milestones)
+# - Milestone 1/2/3/4/5 (choose the one the applies)
+#
+# Which approved additional features have been implemented?
+# (See the project handout for the list of additional features)
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)
+# 3. (fill in the feature, if any)
+# ... (add more if necessary)
+#
+# Any additional information that the TA needs to know:
+# - (write here, if any)
+#
+#####################################################################
